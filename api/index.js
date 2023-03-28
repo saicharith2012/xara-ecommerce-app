dddab67c68af270d3d4be0b1f1f945c4c2f91971
saicharith2012 <saicharithpalabindhela@gmail.com>
2023-03-28 08:51:41 +0530

    used dotenv to hide the secret keys

--- api/index.js
+++ api/index.js
@@ -1,14 +1,20 @@
+// express app
 const express = require('express')
 const app = express()
-
-// mongoose
 const mongoose =  require('mongoose')
-mongoose.connect('mongodb+srv://charith:ztdlQN3fq9TXJKzZ@cluster0.nqmjwur.mongodb.net/?retryWrites=true&w=majority').then(()=>{
+const dotenv = require('dotenv')
+
+// config dotenv
+dotenv.config()
+
+// connecting to mongodb
+mongoose.connect(process.env.MONGO_URL).then(()=>{
     console.log('DBConnection Successful.')
 }).catch((err)=>{
     console.log(err)
 })
 
-app.listen(3000, ()=>{
+// Listening to the requests
+app.listen(process.env.PORT, ()=>{
     console.log('Backend Server is running...')
 })
