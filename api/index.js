26e72dba09be0a344fe6e5e2cd2a144b60e7b053
saicharith2012 <saicharithpalabindhela@gmail.com>
2023-03-28 08:46:01 +0530

    connected to mongodb

--- api/index.js
+++ api/index.js
@@ -1,6 +1,14 @@
 const express = require('express')
 const app = express()
 
+// mongoose
+const mongoose =  require('mongoose')
+mongoose.connect('mongodb+srv://charith:ztdlQN3fq9TXJKzZ@cluster0.nqmjwur.mongodb.net/?retryWrites=true&w=majority').then(()=>{
+    console.log('DBConnection Successful.')
+}).catch((err)=>{
+    console.log(err)
+})
+
 app.listen(3000, ()=>{
     console.log('Backend Server is running...')
 })
