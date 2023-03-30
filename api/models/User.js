712297fc501a422a6d6438947dac2716e7a959ff
saicharith2012 <saicharithpalabindhela@gmail.com>
2023-03-30 21:11:27 +0530

    user model

---
+++ api/models/User.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose')
+
+const UserSchema = new mongoose.Schema(
+    {
+        username:{
+            type:string, required:true, unique:true
+        },
+        email:{
+            type:string, required:true, unique:true
+        },
+        password:{
+            type:string, required:true
+        },
+        isAdmin:{
+            type:boolean,
+            default:false
+        }
+    },
+    {
+        timestamps:true
+    }
+)
+
+module.exports = mongoose.model("User", UserSchema)
