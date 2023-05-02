2aa59f6907cb01d12329e44581fa3a3dac43d9f7
saicharith2012 <saicharithpalabindhela@gmail.com>
2023-05-02 10:04:45 +0530

    changing name

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -71,7 +71,7 @@ const Navbar = () => {
           </SearchContainer>
         </Left>
         <Center>
-          <Logo>LAMA.</Logo>
+          <Logo>XARA.</Logo>
         </Center>
         <Right>
           <MenuItem>REGISTER</MenuItem>
