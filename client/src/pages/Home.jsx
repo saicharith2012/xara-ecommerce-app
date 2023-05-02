a4e36881889385be721d65f72546d33c6d7b96be
saicharith2012 <saicharithpalabindhela@gmail.com>
2023-05-02 10:33:10 +0530

    adding announcement bar at the top

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,10 +1,12 @@
-import React from 'react'
-import Navbar from '../components/Navbar'
+import React from "react"
+import Navbar from "../components/Navbar"
+import Announcement from "../components/Announcement"
 
 const Home = () => {
   return (
     <div>
-      <Navbar/>
+      <Announcement />
+      <Navbar />
     </div>
   )
 }
