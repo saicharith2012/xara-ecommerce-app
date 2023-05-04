cb752335bb15adcf60782b0d50d9aa257ffdb146
saicharith2012 <saicharithpalabindhela@gmail.com>
2023-05-04 16:47:10 +0530

    adding slider items data

---
+++ client/src/data.js
@@ -0,0 +1,23 @@
+export const sliderItems = [
+    {
+        id:1,
+        image:'https://res.cloudinary.com/dq0ljafid/image/upload/v1683080824/yxkcmzf5lrtiecq1pwrl.png',
+        title:"FLASH SALE",
+        desc:"SHOP IN STYLE! GET FLAT 30% OFF FOR LATEST ARRIVALS.",
+        bg:"f7f7ff"
+    },
+    {
+        id:2,
+        image:'https://res.cloudinary.com/dgiksl9k7/image/upload/v1683197765/removal.ai__9b229430-f1af-49cc-9fce-97967db1fe81_XZ697J_x2xvj3.png',
+        title:"REPUBLIC SALE",
+        desc:"GET FLAT 40% ON SELECT ITEMS",
+        bg:"fcf1ed"
+    },
+    {
+        id:3,
+        image:'https://res.cloudinary.com/dgiksl9k7/image/upload/v1683198042/removal.ai__9b9211f0-00ac-42b1-bc4f-f00e0d9ba277_GACOQS_tw8mlj.png',
+        title:"NEW YEAR SALE",
+        desc:"SHOP IN STYLE! GET FLAT 40% OFF ON SELECT ITEMS",
+        bg:"fbf0f4"
+    },
+]
