8bd0d0c3363458a08b550427708c703244ab0cf4
saicharith2012 <saicharithpalabindhela@gmail.com>
2023-05-05 05:41:16 +0530

    added eventlisteners to the slider arrows

--- client/src/components/Slider.jsx
+++ client/src/components/Slider.jsx
@@ -34,7 +34,8 @@ const Arrow = styled.div`
 const Wrapper = styled.div`
   height: 100%;
   display: flex;
-  transform: translateX(0vw);
+  transition: all 0.5s ease;
+  transform: translateX(${(props)=> props.slideIndex * -100}vw);
 `
 const Slide = styled.div`
   height: 100vh;
@@ -84,7 +85,11 @@ const Slider = () => {
 
   const [slideIndex, setSlideIndex] = useState(0)
   const handleClick = (direction) => {
-
+    if(direction === "left"){
+      setSlideIndex(slideIndex > 0 ? slideIndex - 1 : 2)
+    }else{
+      setSlideIndex(slideIndex < 2 ? slideIndex + 1 : 0)
+    }
   }
 
   return (
@@ -93,7 +98,7 @@ const Slider = () => {
         <Arrow direction='left' onClick={()=> handleClick("left")}>
           <ArrowBackIosOutlinedIcon />
         </Arrow>
-        <Wrapper>
+        <Wrapper slideIndex= {slideIndex}>
         {sliderItems.map((item) => (
 
           <Slide bg={item.bg}>
