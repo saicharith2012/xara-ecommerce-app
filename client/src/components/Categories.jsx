62bbc685624cf7a2582eb4684a522b3132eedbcc
saicharith2012 <saicharithpalabindhela@gmail.com>
2023-05-05 14:13:02 +0530

    categories component

---
+++ client/src/components/Categories.jsx
@@ -0,0 +1,23 @@
+import styled from "styled-components"
+import { categories } from "../data"
+import CategoryItem from "./CategoryItem"
+
+
+const Container = styled.div`
+display: flex;
+padding: 20px;
+background-color: floralwhite;
+`
+
+
+const Categories = () => {
+  return (
+    <Container>
+    {categories.map(item=>(
+        <CategoryItem item={item} key={item.id}/>
+    ))}
+    </Container>
+  )
+}
+
+export default Categories
