1ce2be83263cc4e672b39653205f7bf956b42195
saicharith2012 <saicharithpalabindhela@gmail.com>
2023-05-06 10:46:40 +0530

    changing img src of the slider items

--- client/src/data.js
+++ client/src/data.js
@@ -1,42 +1,44 @@
 export const sliderItems = [
-    {
-        id:1,
-        image:'https://res.cloudinary.com/dq0ljafid/image/upload/v1683080824/yxkcmzf5lrtiecq1pwrl.png',
-        title:"FLASH SALE",
-        desc:"SHOP IN STYLE! GET FLAT 30% OFF FOR LATEST ARRIVALS.",
-        bg:"f7f7ff"
-    },
-    {
-        id:2,
-        image:'https://res.cloudinary.com/dgiksl9k7/image/upload/v1683197765/removal.ai__9b229430-f1af-49cc-9fce-97967db1fe81_XZ697J_x2xvj3.png',
-        title:"REPUBLIC SALE",
-        desc:"GET FLAT 40% ON SELECT ITEMS",
-        bg:"fcf1ed"
-    },
-    {
-        id:3,
-        image:'https://res.cloudinary.com/dgiksl9k7/image/upload/v1683198042/removal.ai__9b9211f0-00ac-42b1-bc4f-f00e0d9ba277_GACOQS_tw8mlj.png',
-        title:"NEW YEAR SALE",
-        desc:"SHOP IN STYLE! GET FLAT 40% OFF ON SELECT ITEMS",
-        bg:"fbf0f4"
-    },
+  {
+    id: 1,
+    image:
+      "https://res.cloudinary.com/dq0ljafid/image/upload/v1683080824/yxkcmzf5lrtiecq1pwrl.png",
+    title: "FLASH SALE",
+    desc: "SHOP IN STYLE! GET FLAT 30% OFF FOR LATEST ARRIVALS.",
+    bg: "f7f7ff",
+  },
+  {
+    id: 2,
+    image:
+      "https://res.cloudinary.com/dgiksl9k7/image/upload/v1683348420/xara-ecommerce-app/_removal.ai__9b229430-f1af-49cc-9fce-97967db1fe81_XZ697J2-removebg-preview_flpjb6.png",
+    title: "REPUBLIC SALE",
+    desc: "GET FLAT 40% ON SELECT ITEMS",
+    bg: "fcf1ed",
+  },
+  {
+    id: 3,
+    image:
+      "https://res.cloudinary.com/dgiksl9k7/image/upload/v1683348416/xara-ecommerce-app/_removal.ai__9b9211f0-00ac-42b1-bc4f-f00e0d9ba277_GACOQS-removebg-preview_abxzcj.png",
+    title: "NEW YEAR SALE",
+    desc: "SHOP IN STYLE! GET FLAT 40% OFF ON SELECT ITEMS",
+    bg: "fbf0f4",
+  },
 ]
 
 export const categories = [
-    {
-      id: 1,
-      img: "https://res.cloudinary.com/dgiksl9k7/image/upload/v1683248676/xara-ecommerce-app/mens-black-suit-styles-business-professional-ideas_ymnzhw.jpg",
-      title: "MEN IN STYLE!",
-    },
-    {
-      id: 2,
-      img: "https://res.cloudinary.com/dgiksl9k7/image/upload/v1683248676/xara-ecommerce-app/d5e34a597407651fe900f9f80d7d6fad_gahknf.jpg",
-      title: "LADY LOVE!",
-    },
-    {
-      id: 3,
-      img: "https://res.cloudinary.com/dgiksl9k7/image/upload/v1683248676/xara-ecommerce-app/leatherjacketsmain1_g3qggu.jpg",
-      title: "LIGHT JACKETS",
-    },
-  ];
-
+  {
+    id: 1,
+    img: "https://res.cloudinary.com/dgiksl9k7/image/upload/v1683248676/xara-ecommerce-app/mens-black-suit-styles-business-professional-ideas_ymnzhw.jpg",
+    title: "MEN IN STYLE!",
+  },
+  {
+    id: 2,
+    img: "https://res.cloudinary.com/dgiksl9k7/image/upload/v1683248676/xara-ecommerce-app/d5e34a597407651fe900f9f80d7d6fad_gahknf.jpg",
+    title: "LADY LOVE!",
+  },
+  {
+    id: 3,
+    img: "https://res.cloudinary.com/dgiksl9k7/image/upload/v1683248676/xara-ecommerce-app/leatherjacketsmain1_g3qggu.jpg",
+    title: "LIGHT JACKETS",
+  },
+]
