d47759d7c5517b7184edcb4a3a09c16757cdb6df
saicharith2012 <saicharithpalabindhela@gmail.com>
2023-05-07 07:50:02 +0530

    adding background color to the navbar

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -6,6 +6,7 @@ import ShoppingCartOutlinedIcon from "@mui/icons-material/ShoppingCartOutlined"
 
 const Container = styled.div`
   height: 60px;
+  background-color: white;
 `;
 
 const Wrapper = styled.div`
