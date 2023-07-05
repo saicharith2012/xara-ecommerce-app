6ffde6c51806a866cd82c2d1ce29b81876843971
saicharith2012 <saicharithpalabindhela@gmail.com>
2023-07-05 12:58:58 +0530

    formatting the code

--- client/src/pages/Cart.jsx
+++ client/src/pages/Cart.jsx
@@ -9,12 +9,12 @@ const Container = styled.div``
 
 const Wrapper = styled.div`
   padding: 20px;
-  `
+`
 
 const Title = styled.h1`
   font-weight: 300;
   text-align: center;
-  `
+`
 
 const Top = styled.div`
   display: flex;
@@ -52,75 +52,68 @@ const Info = styled.div`
 `
 
 const Product = styled.div`
-display: flex;
-justify-content: space-between;
-margin: 5px 0px;
+  display: flex;
+  justify-content: space-between;
+  margin: 5px 0px;
 `
 
 const ProductDetail = styled.div`
-flex: 2;
-display: flex;
+  flex: 2;
+  display: flex;
 `
 
 const Image = styled.img`
-width: 150px;
-height: 150px;
-object-fit: cover;
+  width: 150px;
+  height: 150px;
+  object-fit: cover;
 `
 
 const Details = styled.div`
-padding: 20px;
-display: flex;
-flex-direction: column;
-justify-content: space-around;
+  padding: 20px;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
 `
 
-const ProductName = styled.span`
-
-`
-const ProductID = styled.span`
-
-`
+const ProductName = styled.span``
+const ProductID = styled.span``
 const ProductColor = styled.div`
-width: 20px;
-height: 20px;
-border-radius: 50%;
-background-color: ${props=> props.color};
-
+  width: 20px;
+  height: 20px;
+  border-radius: 50%;
+  background-color: ${(props) => props.color};
 `
-const ProductSize = styled.span`
-
-`
+const ProductSize = styled.span``
 const PriceDetail = styled.div`
-flex: 2;
-display: flex;
-flex-direction: column;
-align-items: center;
-justify-content: center;
+  flex: 2;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
 `
 
-const ProductAmountContainer =styled.div`
-display: flex;
-align-items: center;
-margin-bottom: 15px;
+const ProductAmountContainer = styled.div`
+  display: flex;
+  align-items: center;
+  margin-bottom: 15px;
 `
 
 const ProductAmount = styled.div`
-font-size: 20px;
-margin: 5px;
-border: 1px solid black;
-padding: 0px 5px;
+  font-size: 20px;
+  margin: 5px;
+  border: 1px solid black;
+  padding: 0px 5px;
 `
 const ProductPrice = styled.div`
-font-size: 25px;
+  font-size: 25px;
 `
 
 const Hr = styled.hr`
-background-color: #eee;
-border: none;
-height: 1px;
-width: 98%;
-align-self: center;
+  background-color: #eee;
+  border: none;
+  height: 1px;
+  width: 98%;
+  align-self: center;
 `
 
 const Summary = styled.div`
@@ -129,39 +122,34 @@ const Summary = styled.div`
   border-radius: 10px;
   padding: 20px;
   height: fit-content;
-  `
+`
 
 const SummaryTitle = styled.h1`
-font-weight: 200;
+  font-weight: 200;
 `
 
 const SummaryItem = styled.div`
-margin: 30px 0px;
-display: flex;
-justify-content: space-between;
-font-weight: ${props=>props.type === "total" && "500"};
-font-size: ${props=>props.type === "total" && "24px"};
-
+  margin: 30px 0px;
+  display: flex;
+  justify-content: space-between;
+  font-weight: ${(props) => props.type === "total" && "500"};
+  font-size: ${(props) => props.type === "total" && "24px"};
 `
 
-const SummaryItemText = styled.span`
+const SummaryItemText = styled.span``
 
-`
-
-const SummaryItemPrice = styled.span`
-
-`
+const SummaryItemPrice = styled.span``
 const Button = styled.button`
-width:100%;
-padding: 10px;
-background-color: black;
-color:white;
-font-weight: 600;
-border: none;`
-
+  width: 100%;
+  padding: 10px;
+  background-color: black;
+  color: white;
+  font-weight: 600;
+  border: none;
+`
 
 const Cart = () => {
-    return (
+  return (
     <Container>
       <Navbar />
       <Announcement />
@@ -178,63 +166,81 @@ const Cart = () => {
         <Bottom>
           <Info>
             <Product>
-                <ProductDetail>
-                    <Image src="https://res.cloudinary.com/dgiksl9k7/image/upload/v1683351758/xara-ecommerce-app/products/1856083330_6_1_1_v3ieju.jpg"/>
-                    <Details>
-                        <ProductName><b>Product: </b>JESSIE QUINN PEBBLES CHAIN</ProductName>
-                        <ProductID><b>ID: </b>87564221389</ProductID>
-                        <ProductSize><b>Size: </b>NONE</ProductSize>
-                        <ProductColor color="orange"></ProductColor>
-                    </Details>
-                </ProductDetail>
-                <PriceDetail>
+              <ProductDetail>
+                <Image src='https://res.cloudinary.com/dgiksl9k7/image/upload/v1683351758/xara-ecommerce-app/products/1856083330_6_1_1_v3ieju.jpg' />
+                <Details>
+                  <ProductName>
+                    <b>Product: </b>JESSIE QUINN PEBBLES CHAIN
+                  </ProductName>
+                  <ProductID>
+                    <b>ID: </b>87564221389
+                  </ProductID>
+                  <ProductSize>
+                    <b>Size: </b>NONE
+                  </ProductSize>
+                  <ProductColor color='orange'></ProductColor>
+                </Details>
+              </ProductDetail>
+              <PriceDetail>
                 <ProductAmountContainer>
-                    <AddIcon/>
-                    <ProductAmount>3</ProductAmount>
-                    <RemoveIcon/>
+                  <AddIcon />
+                  <ProductAmount>3</ProductAmount>
+                  <RemoveIcon />
                 </ProductAmountContainer>
                 <ProductPrice>Rs. 800</ProductPrice>
-                </PriceDetail>
+              </PriceDetail>
             </Product>
-            <Hr/>
+            <Hr />
             <Product>
-                <ProductDetail>
-                    <Image src="https://res.cloudinary.com/dgiksl9k7/image/upload/v1683351758/xara-ecommerce-app/products/1856083330_6_1_1_v3ieju.jpg"/>
-                    <Details>
-                        <ProductName><b>Product: </b>JESSIE QUINN PEBBLES CHAIN</ProductName>
-                        <ProductID><b>ID: </b>87564221389</ProductID>
-                        <ProductSize><b>Size: </b>NONE</ProductSize>
-                        <ProductColor color="orange"></ProductColor>
-                    </Details>
-                </ProductDetail>
-                <PriceDetail>
+              <ProductDetail>
+                <Image src='https://res.cloudinary.com/dgiksl9k7/image/upload/v1683351758/xara-ecommerce-app/products/1856083330_6_1_1_v3ieju.jpg' />
+                <Details>
+                  <ProductName>
+                    <b>Product: </b>JESSIE QUINN PEBBLES CHAIN
+                  </ProductName>
+                  <ProductID>
+                    <b>ID: </b>87564221389
+                  </ProductID>
+                  <ProductSize>
+                    <b>Size: </b>NONE
+                  </ProductSize>
+                  <ProductColor color='orange'></ProductColor>
+                </Details>
+              </ProductDetail>
+              <PriceDetail>
                 <ProductAmountContainer>
-                    <AddIcon/>
-                    <ProductAmount>3</ProductAmount>
-                    <RemoveIcon/>
+                  <AddIcon />
+                  <ProductAmount>3</ProductAmount>
+                  <RemoveIcon />
                 </ProductAmountContainer>
                 <ProductPrice>Rs. 800</ProductPrice>
-                </PriceDetail>
+              </PriceDetail>
             </Product>
-            <Hr/>
+            <Hr />
             <Product>
-                <ProductDetail>
-                    <Image src="https://res.cloudinary.com/dgiksl9k7/image/upload/v1683351758/xara-ecommerce-app/products/1856083330_6_1_1_v3ieju.jpg"/>
-                    <Details>
-                        <ProductName><b>Product: </b>JESSIE QUINN PEBBLES CHAIN</ProductName>
-                        <ProductID><b>ID: </b>87564221389</ProductID>
-                        <ProductSize><b>Size: </b>NONE</ProductSize>
-                        <ProductColor color="orange"></ProductColor>
-                    </Details>
-                </ProductDetail>
-                <PriceDetail>
+              <ProductDetail>
+                <Image src='https://res.cloudinary.com/dgiksl9k7/image/upload/v1683351758/xara-ecommerce-app/products/1856083330_6_1_1_v3ieju.jpg' />
+                <Details>
+                  <ProductName>
+                    <b>Product: </b>JESSIE QUINN PEBBLES CHAIN
+                  </ProductName>
+                  <ProductID>
+                    <b>ID: </b>87564221389
+                  </ProductID>
+                  <ProductSize>
+                    <b>Size: </b>NONE
+                  </ProductSize>
+                  <ProductColor color='orange'></ProductColor>
+                </Details>
+              </ProductDetail>
+              <PriceDetail>
                 <ProductAmountContainer>
-                    <AddIcon/>
-                    <ProductAmount>3</ProductAmount>
-                    <RemoveIcon/>
+                  <AddIcon />
+                  <ProductAmount>3</ProductAmount>
+                  <RemoveIcon />
                 </ProductAmountContainer>
                 <ProductPrice>Rs. 1100</ProductPrice>
-                </PriceDetail>
+              </PriceDetail>
             </Product>
           </Info>
           <Summary>
@@ -251,7 +257,7 @@ const Cart = () => {
               <SummaryItemText>Shipping Discount</SummaryItemText>
               <SummaryItemPrice>Rs. 99</SummaryItemPrice>
             </SummaryItem>
-            <SummaryItem type="total">
+            <SummaryItem type='total'>
               <SummaryItemText>Total</SummaryItemText>
               <SummaryItemPrice>Rs. 1900</SummaryItemPrice>
             </SummaryItem>
