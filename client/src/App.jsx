c28eddfcf88a0bbe87eef047dc43621eb88795d1
saicharith2012 <saicharithpalabindhela@gmail.com>
2023-07-06 16:14:47 +0530

    preparing for build

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,10 @@
-import Home from "./pages/Home";
-import ProductList from "./pages/ProductList";
-import Product from "./pages/Product";
+// import Home from "./pages/Home";
+// import ProductList from "./pages/ProductList";
+// import Product from "./pages/Product";
 
 import Cart from "./pages/Cart";
-import Login from "./pages/Login";
-import Register from "./pages/Register";
+// import Login from "./pages/Login";
+// import Register from "./pages/Register";
 
 const App = () => {
     return <Cart/>
