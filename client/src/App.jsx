7f2f2984610ecbee17b7537daba5c4652ff2c55a
saicharith2012 <saicharithpalabindhela@gmail.com>
2023-07-13 06:38:01 +0530

    making register as the landing page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,13 @@
-import Home from "./pages/Home";
+// import Home from "./pages/Home";
 // import ProductList from "./pages/ProductList";
 // import Product from "./pages/Product";
 
 // import Cart from "./pages/Cart";
 // import Login from "./pages/Login";
-// import Register from "./pages/Register";
+import Register from "./pages/Register";
 
 const App = () => {
-    return <Home/>
+    return <Register/>
 }
 
 
