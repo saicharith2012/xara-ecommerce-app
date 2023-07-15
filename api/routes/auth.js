8043f9612139089bf69e56e58fcd6005b8b654d3
saicharith2012 <saicharithpalabindhela@gmail.com>
2023-07-15 17:56:53 +0530

    encrypted password with cryptoJS AES

--- api/routes/auth.js
+++ api/routes/auth.js
@@ -1,22 +1,27 @@
-const User = require('../models/User')
+const CryptoJS = require("crypto-js")
+const User = require("../models/User")
+const router = require("express").Router()
+const dotenv = require("dotenv")
 
-const router = require('express').Router()
-
+dotenv.config()
 
 //REGISTER
-router.post('/register', async (req,res)=>{
-    const newUser =  new User({
-        username: req.body.username,
-        email: req.body.email,
-        password: req.body.password,
-    })
+router.post("/register", async (req, res) => {
+  const newUser = new User({
+    username: req.body.username,
+    email: req.body.email,
+    password: CryptoJS.AES.encrypt(
+      req.body.password,
+      process.env.PASS_KEY
+    ).toString(),
+  })
 
-    try{
-        const savedUser = await newUser.save()
-        res.status(201).json(savedUser)
-    }catch(err){
-        res.status(500).json(err)
-    }
+  try {
+    const savedUser = await newUser.save()
+    res.status(201).json(savedUser)
+  } catch (err) {
+    res.status(500).json(err)
+  }
 })
 
 module.exports = router
