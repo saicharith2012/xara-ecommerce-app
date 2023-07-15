dca90a7304c2438ff13aa7db9aeab5b07405e976
saicharith2012 <saicharithpalabindhela@gmail.com>
2023-07-15 17:45:06 +0530

    post req for register

--- api/routes/auth.js
+++ api/routes/auth.js
@@ -1,43 +1,19 @@
-const router = require('express').Router()
 const User = require('../models/User')
-const CryptoJS = require("crypto-js");
-const dotenv = require("dotenv")
 
-dotenv.config()
+const router = require('express').Router()
 
 
-// REGISTER
+//REGISTER
 router.post('/register', async (req,res)=>{
-    const newUser = new User({
-        username: req.body.username, //unique
-        email: req.body.email, //unique -- throws err if repeated
-        password: CryptoJS.AES.encrypt( req.body.password, process.env.PASS_SEC ).toString()
+    const newUser =  new User({
+        username: req.body.username,
+        email: req.body.email,
+        password: req.body.password,
     })
 
     try{
         const savedUser = await newUser.save()
-        res.status(200).json(savedUser)
-    }catch(err){
-        res.status(500).json(err)
-    }
-})
-
-// LOGIN
-router.post('/login', async (req,res)=>{
-    try{
-        const user = await User.findOne({username: req.body.username})
-        !user && res.status(401).json('Wrong Credentials!')
-
-        const hashedPassword = CryptoJS.AES.decrypt(user.password, process.env.PASS_SEC);
-        const originalPassword = hashedPassword.toString(CryptoJS.enc.Utf8)
-
-        originalPassword !== req.body.password && res.status(401).json("Wrong Credentials!")
-
-        const {password, ...others} = user._doc
-
-        res.status(200).json(others)
-
-        
+        res.status(201).json(savedUser)
     }catch(err){
         res.status(500).json(err)
     }
