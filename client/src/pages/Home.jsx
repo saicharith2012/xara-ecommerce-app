22f8b8b5313e15057f43e91935ca0c80b65d1232
saicharith2012 <saicharithpalabindhela@gmail.com>
2024-06-11 11:06:34 +0530

    Adding heading for the latest products

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,24 +1,35 @@
-import React from "react"
-import Navbar from "../components/Navbar"
-import Announcement from "../components/Announcement"
-import Slider from "../components/Slider"
-import Categories from "../components/Categories"
-import Products from "../components/Products"
-import Newsletter from "../components/Newsletter"
-import Footer from "../components/Footer"
+import React from "react";
+import Navbar from "../components/Navbar";
+import Announcement from "../components/Announcement";
+import Slider from "../components/Slider";
+import Categories from "../components/Categories";
+import Products from "../components/Products";
+import Newsletter from "../components/Newsletter";
+import Footer from "../components/Footer";
 
 const Home = () => {
   return (
-    <div>
+    <div style={{ paddingTop: "60px" }}>
       <Announcement />
       <Navbar />
-      <Slider/>
-      <Categories/>
-      <Products/>
-      <Newsletter/>
-      <Footer/>
+      <Slider />
+      <Categories />
+      <div
+        className="products-heading"
+        style={{
+          textAlign: "center",
+          fontSize: "70px",
+          fontWeight: "1000",
+          padding: "30px 0px 50px 0px",
+        }}
+      >
+        <p>LATEST PRODUCTS.</p>
+      </div>
+      <Products category={"new"} filters={[]} />
+      <Newsletter />
+      <Footer />
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
