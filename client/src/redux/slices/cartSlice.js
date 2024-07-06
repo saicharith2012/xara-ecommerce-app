9296e8a7863dd4c810f84505705074da03496748
saicharith2012 <saicharithpalabindhela@gmail.com>
2024-07-06 22:52:42 +0530

    reducer for clearing cart

--- client/src/redux/slices/cartSlice.js
+++ client/src/redux/slices/cartSlice.js
@@ -13,9 +13,14 @@ const cartSlice = createSlice({
       state.products.push(action.payload);
       state.total += action.payload.price * action.payload.productQuantity;
     },
+    clearCart: (state) => {
+      state.products = [];
+      state.total = 0;
+      state.quantity = 0;
+    }
   },
 });
 
 
-export const {addProduct} = cartSlice.actions
+export const {addProduct, clearCart} = cartSlice.actions
 export default cartSlice.reducer;
