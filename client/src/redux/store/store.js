091cbeeb1448749cde73eba16ca9587e6f36cbbe
saicharith2012 <saicharithpalabindhela@gmail.com>
2024-07-06 22:53:56 +0530

    adding persistance to the redux state

--- client/src/redux/store/store.js
+++ client/src/redux/store/store.js
@@ -1,11 +1,45 @@
-import { configureStore } from '@reduxjs/toolkit';
-import cartReducer from "../slices/cartSlice.js"
-import authReducer from "../slices/authSlice.js"
+import { configureStore, combineReducers } from '@reduxjs/toolkit';
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist';
+import storage from 'redux-persist/lib/storage'; // defaults to localStorage for web
 
-export default configureStore({
-    reducer: {
-        cart: cartReducer,
-        auth: authReducer
-    },
+import cartReducer from "../slices/cartSlice.js";
+import authReducer from "../slices/authSlice.js";
+
+// persist config
+const persistConfig = {
+  key: 'root',
+  version: 1,
+  storage,
+};
+
+// Combining reducers
+const rootReducer = combineReducers({
+  cart: cartReducer,
+  auth: authReducer,
 });
 
+// persisted reducer
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+// Configure the store with the persisted reducer
+const store = configureStore({
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
+});
+
+export const persistor = persistStore(store);
+export default store;
