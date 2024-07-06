1d31213592294c6a112576b353edadd4e614ec64
saicharith2012 <saicharithpalabindhela@gmail.com>
2024-07-06 20:17:09 +0530

    cors origin

--- api/src/app.js
+++ api/src/app.js
@@ -10,6 +10,8 @@ const app = express();
 app.use(
   cors({
     origin: process.env.CORS_ORIGIN,
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true,
   })
 );
