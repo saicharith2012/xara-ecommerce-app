fa3bb0b7b7590b1ebe293164f940f80fdb1d2cc6
saicharith2012 <saicharithpalabindhela@gmail.com>
2024-07-07 10:39:15 +0530

    register user logic

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,5 +1,8 @@
 import styled from "styled-components";
 import { mobile, medium } from "../responsive";
+import { useState } from "react";
+import { publicRequest } from "../requestMethods";
+import { useNavigate } from "react-router-dom";
 
 const Container = styled.div`
   width: 100vw;
@@ -29,8 +32,8 @@ const Heading = styled.div`
 const Wrapper = styled.div`
   width: 50%;
   padding: 20px 40px 30px 40px;
-  background-color: rgba(255,255,255,0.6);
-  box-shadow: 3px 3px 10px rgba(0,0,0,0.2);
+  background-color: rgba(255, 255, 255, 0.6);
+  box-shadow: 3px 3px 10px rgba(0, 0, 0, 0.2);
   border-radius: 10px;
   ${mobile({ width: "75%", margin: 0, padding: "20px 40px 30px 40px" })}
   ${medium({ width: "65%", margin: 0, padding: "20px 40px 30px 40px" })}
@@ -65,6 +68,8 @@ const Agreement = styled.span`
   margin: 30px 0px;
 `;
 
+const Checkbox = styled.input``;
+
 const Button = styled.button`
   width: 40%;
   border: none;
@@ -75,26 +80,101 @@ const Button = styled.button`
   font-weight: 700;
   border-radius: 5px;
   margin-bottom: 10px;
+  margin-right: 12px;
 `;
 
+const ErrorMessage = styled.div`
+  padding: 12px 12px 12px 0px;
+`
+
 const Register = () => {
+  const [firstname, setFirstname] = useState("");
+  const [lastname, setLastname] = useState("");
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const [errorMessage, setErrorMessage] = useState("");
+
+  const navigate = useNavigate()
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    if (password !== confirmPassword) {
+      setErrorMessage("Passwords do not match.");
+      return;
+    }
+
+    try {
+      const response = await publicRequest.post("users/register",{
+        firstname, lastname, username, email, password
+      }, {
+        withCredentials: true
+      })
+
+      console.log(response)
+
+      navigate("/login")
+    } catch (error) {
+      setErrorMessage("")
+    }
+  };
+
   return (
     <Container>
       <Wrapper>
         <Heading>élegance</Heading>
         <Title>CREATE ACCOUNT</Title>
-        <Form>
-          <Input placeholder="first name" />
-          <Input placeholder="last name" />
-          <Input placeholder="username" />
-          <Input placeholder="email" />
-          <Input placeholder="password" />
-          <Input placeholder="confirm password" />
+        <Form onSubmit={handleSubmit}>
+          <Input
+            placeholder="first name"
+            value={firstname}
+            type="text"
+            onChange={(e) => setFirstname(e.target.value)}
+            required
+          />
+          <Input
+            placeholder="last name"
+            value={lastname}
+            type="text"
+            onChange={(e) => setLastname(e.target.value)}
+            required
+          />
+          <Input
+            placeholder="username"
+            value={username}
+            type="text"
+            onChange={(e) => setUsername(e.target.value)}
+            required
+          />
+          <Input
+            placeholder="email"
+            value={email}
+            type="email"
+            onChange={(e) => setEmail(e.target.value)}
+            required
+          />
+          <Input
+            placeholder="password"
+            value={password}
+            type="password"
+            onChange={(e) => setPassword(e.target.value)}
+            required
+          />
+          <Input
+            placeholder="confirm password"
+            value={confirmPassword}
+            type="password"
+            onChange={(e) => setConfirmPassword(e.target.value)}
+            required
+          />
           <Agreement>
-            By creating an account, I consent to the processing of my personal
-            data in accordance with the <b>PRIVACY POLICY</b>
+            <Checkbox type="checkbox" required></Checkbox> By creating an
+            account, I consent to the processing of my personal data in
+            accordance with the <b>PRIVACY POLICY</b>
           </Agreement>
           <Button>REGISTER</Button>
+          {errorMessage && <ErrorMessage>{errorMessage}</ErrorMessage>}
         </Form>
       </Wrapper>
     </Container>
