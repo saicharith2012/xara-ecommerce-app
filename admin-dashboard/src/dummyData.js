2172f87a02de4294518800d7d07a61803ef530f4
saicharith2012 <saicharithpalabindhela@gmail.com>
2024-07-11 16:03:20 +0530

    dummy data

--- admin-dashboard/src/dummyData.js
+++ admin-dashboard/src/dummyData.js
@@ -49,7 +49,26 @@ export const userData = [
   },
 ];
 
-export const rows = [
+export const productData = [
+  {
+    name: "Apr",
+    Sales: 178,
+  },
+  {
+    name: "May",
+    Sales: 89,
+  },
+  {
+    name: "Jun",
+    Sales: 325,
+  },
+  {
+    name: "Jul",
+    Sales: 216,
+  },
+];
+
+export const userRows = [
   {
     id: 1,
     username: "Jon Snow",
@@ -139,5 +158,170 @@ export const rows = [
     avatar:
       "https://pbs.twimg.com/profile_images/1723501431842594816/VIOR52K3_400x400.jpg",
     transaction: "Rs. 4530",
+  },
+  {
+    id: 11,
+    username: "Jon Snow",
+    email: "jonsnow@got.com",
+    status: "Active",
+    avatar:
+      "https://pbs.twimg.com/profile_images/1723501431842594816/VIOR52K3_400x400.jpg",
+    transaction: "Rs. 4530",
+  },
+  {
+    id: 12,
+    username: "Jon Snow",
+    email: "jonsnow@got.com",
+    status: "Active",
+    avatar:
+      "https://pbs.twimg.com/profile_images/1723501431842594816/VIOR52K3_400x400.jpg",
+    transaction: "Rs. 4530",
+  },
+  {
+    id: 13,
+    username: "Jon Snow",
+    email: "jonsnow@got.com",
+    status: "Active",
+    avatar:
+      "https://pbs.twimg.com/profile_images/1723501431842594816/VIOR52K3_400x400.jpg",
+    transaction: "Rs. 4530",
+  },
+  {
+    id: 14,
+    username: "Jon Snow",
+    email: "jonsnow@got.com",
+    status: "Active",
+    avatar:
+      "https://pbs.twimg.com/profile_images/1723501431842594816/VIOR52K3_400x400.jpg",
+    transaction: "Rs. 4530",
+  },
+];
+
+export const productRows = [
+  {
+    id: 1,
+    name: "Blue Suit",
+    image:
+      "https://res.cloudinary.com/dgiksl9k7/image/upload/v1683351711/xara-ecommerce-app/products/3736044403_1_1_1_nnhhqi.jpg",
+    status: "Active",
+    stock: 123,
+    price: "Rs. 4530",
+  },
+  {
+    id: 2,
+    name: "Blue Suit",
+    image:
+      "https://res.cloudinary.com/dgiksl9k7/image/upload/v1683351711/xara-ecommerce-app/products/3736044403_1_1_1_nnhhqi.jpg",
+    status: "Active",
+    stock: 123,
+    price: "Rs. 4530",
+  },
+  {
+    id: 3,
+    name: "Blue Suit",
+    image:
+      "https://res.cloudinary.com/dgiksl9k7/image/upload/v1683351711/xara-ecommerce-app/products/3736044403_1_1_1_nnhhqi.jpg",
+    status: "Active",
+    stock: 123,
+    price: "Rs. 4530",
+  },
+  {
+    id: 4,
+    name: "Blue Suit",
+    image:
+      "https://res.cloudinary.com/dgiksl9k7/image/upload/v1683351711/xara-ecommerce-app/products/3736044403_1_1_1_nnhhqi.jpg",
+    status: "Active",
+    stock: 123,
+    price: "Rs. 4530",
+  },
+  {
+    id: 5,
+    name: "Blue Suit",
+    image:
+      "https://res.cloudinary.com/dgiksl9k7/image/upload/v1683351711/xara-ecommerce-app/products/3736044403_1_1_1_nnhhqi.jpg",
+    status: "Active",
+    stock: 123,
+    price: "Rs. 4530",
+  },
+  {
+    id: 6,
+    name: "Blue Suit",
+    image:
+      "https://res.cloudinary.com/dgiksl9k7/image/upload/v1683351711/xara-ecommerce-app/products/3736044403_1_1_1_nnhhqi.jpg",
+    status: "Active",
+    stock: 123,
+    price: "Rs. 4530",
+  },
+  {
+    id: 7,
+    name: "Blue Suit",
+    image:
+      "https://res.cloudinary.com/dgiksl9k7/image/upload/v1683351711/xara-ecommerce-app/products/3736044403_1_1_1_nnhhqi.jpg",
+    status: "Active",
+    stock: 123,
+    price: "Rs. 4530",
+  },
+  {
+    id: 8,
+    name: "Blue Suit",
+    image:
+      "https://res.cloudinary.com/dgiksl9k7/image/upload/v1683351711/xara-ecommerce-app/products/3736044403_1_1_1_nnhhqi.jpg",
+    status: "Active",
+    stock: 123,
+    price: "Rs. 4530",
+  },
+  {
+    id: 9,
+    name: "Blue Suit",
+    image:
+      "https://res.cloudinary.com/dgiksl9k7/image/upload/v1683351711/xara-ecommerce-app/products/3736044403_1_1_1_nnhhqi.jpg",
+    status: "Active",
+    stock: 123,
+    price: "Rs. 4530",
+  },
+  {
+    id: 10,
+    name: "Blue Suit",
+    image:
+      "https://res.cloudinary.com/dgiksl9k7/image/upload/v1683351711/xara-ecommerce-app/products/3736044403_1_1_1_nnhhqi.jpg",
+    status: "Active",
+    stock: 123,
+    price: "Rs. 4530",
+  },
+  {
+    id: 11,
+    name: "Blue Suit",
+    image:
+      "https://res.cloudinary.com/dgiksl9k7/image/upload/v1683351711/xara-ecommerce-app/products/3736044403_1_1_1_nnhhqi.jpg",
+    status: "Active",
+    stock: 123,
+    price: "Rs. 4530",
+  },
+  {
+    id: 12,
+    name: "Blue Suit",
+    image:
+      "https://res.cloudinary.com/dgiksl9k7/image/upload/v1683351711/xara-ecommerce-app/products/3736044403_1_1_1_nnhhqi.jpg",
+    status: "Active",
+    stock: 123,
+    price: "Rs. 4530",
+  },
+  {
+    id: 13,
+    name: "Blue Suit",
+    image:
+      "https://res.cloudinary.com/dgiksl9k7/image/upload/v1683351711/xara-ecommerce-app/products/3736044403_1_1_1_nnhhqi.jpg",
+    status: "Active",
+    stock: 123,
+    price: "Rs. 4530",
+  },
+  {
+    id: 14,
+    name: "Blue Suit",
+    image:
+      "https://res.cloudinary.com/dgiksl9k7/image/upload/v1683351711/xara-ecommerce-app/products/3736044403_1_1_1_nnhhqi.jpg",
+    status: "Active",
+    stock: 123,
+    price: "Rs. 4530",
   },
 ];
