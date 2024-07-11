dfdf08ef00aea8b8e3b9dc3f32b8153199d444ee
saicharith2012 <saicharithpalabindhela@gmail.com>
2024-07-11 19:47:50 +0530

    ngrok secure tunneling

--- api/src/index.js
+++ api/src/index.js
@@ -1,6 +1,7 @@
 import app from "./app.js";
 import connectDB from "./db/index.js";
 import dotenv from "dotenv";
+import ngrok from "@ngrok/ngrok";
 
 dotenv.config({
   path: "../env",
@@ -10,8 +11,15 @@ const port = process.env.PORT || 4000;
 
 connectDB()
   .then(() => {
-    app.listen(port, () => {
-      console.log(`Server is running on port: ${port}`);
+    app.listen(port, async () => {
+      console.log(`Server is running on http://localhost:${port}`);
+
+      const listener = await ngrok.forward({
+        addr: port,
+        authtoken: process.env.NGROK_AUTH_TOKEN,
+      });
+
+      console.log(`Ingress established at: ${listener.url()}`);
     });
   })
   .catch((error) => {
