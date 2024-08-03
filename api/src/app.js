deebc8c68904dd7a9e349562046220b7d3c152a5
saicharith2012 <saicharithpalabindhela@gmail.com>
2024-08-03 13:18:08 +0530

    removing dev servers from cors

--- api/src/app.js
+++ api/src/app.js
@@ -9,7 +9,7 @@ const app = express();
 //  allowing the requests with credentials and authorization headers
 app.use(
   cors({
-    origin: [process.env.CORS_ORIGIN, process.env.CORS_ORIGIN_ADMIN, process.env.CORS_ORIGIN_FRONTEND_DEPLOYMENT],
+    origin: [process.env.CORS_ORIGIN_FRONTEND_DEPLOYMENT],
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true,
